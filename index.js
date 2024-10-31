1b13337fd2962958531c9b4a34c3a462e1951c52
awannoman <160035058+awannoman@users.noreply.github.com>
2024-10-31 17:54:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+// Array Part 01
+let num =[0,1,2,3,4,5];
+// let name = ["noman","daniyal"]
+// let num2 = new Array(1,2,3,4)
+
+// num.push(6,7,8,9);
+// num.pop()
+
+// num.unshift(9)
+// num.shift()
+// console.log(num);
+
+// console.log(num.includes(4))
+// console.log(num.indexOf(5));
+
+// console.log(num);
+// let newArr = num.join()
+// console.log(newArr);
+
+// console.log("A ",num);
+// let n1 = num.slice(1,3)
+
+// console.log(n1)
+// console.log("b ",num);
+
+// let n2 = num.splice(1,3)
+// console.log("c ",num);
+// console.log(n2)
+
+// Array Part 02
+
+let heros = ['noman','daniyal','umer']
+let proHeros = ['adnan', 'nomi','ali']
+// heros.push(proHeros)
+// console.log(heros)
+// console.log(heros[3][0]);
+// concat method
+// let newHeros = heros.concat(proHeros)
+// console.log(newHeros)
+// spride method
+// let newHeros = [...heros, ...proHeros]
+// console.log(newHeros);
+ 
+// let arr = [1,2,3, [4,5,6,], 7, [6, 7, [4,5]] ]
+// let newArr = arr.flat(Infinity)
+// console.log(newArr);
+
+// console.log(Array.isArray(1.2,3,4));
+// console.log(Array.from("noman"));
+// console.log(Array.from({name: "noman"}));
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+
+// console.log(Array.of(score1,score2,score3));
+
